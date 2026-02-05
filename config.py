6a080ad8bc8f7084f0a11d12b45911bbc4c9926a
appolinair2355 <felicitationsbien990@gmail.com>
2026-02-05 01:06:54 +0100

    Update PREDICTION_CHANNEL_ID in config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ADMIN_ID = 1190237801
 
 # Canaux (À MODIFIER selon tes besoins)
 SOURCE_CHANNEL_ID = -1002682552255      # Canal qui reçoit les jeux
-PREDICTION_CHANNEL_ID = -1002433207574  # Canal des prédictions
+PREDICTION_CHANNEL_ID = -1003502536129  # Canal des prédictions
 VIP_CHANNEL_ID = -1003502536129         # Canal VIP privé
 VIP_CHANNEL_LINK = "https://t.me/+3pHxyUtjt34zMzg0"
 
